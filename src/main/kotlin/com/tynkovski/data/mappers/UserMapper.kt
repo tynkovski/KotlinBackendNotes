b401587c81acd6ddeb70cca022d08632d31aca3b
Stanislav Tynkovski <56252471+tynkovski@users.noreply.github.com>
2023-08-02 00:01:23 +0400

    mapper

--- src/main/kotlin/com/tynkovski/data/mappers/UserMapper.kt
+++ src/main/kotlin/com/tynkovski/data/mappers/UserMapper.kt
@@ -3,6 +3,4 @@ package com.tynkovski.data.mappers
 import com.tynkovski.data.entities.User
 import com.tynkovski.data.responses.UserResponse
 
-fun userMapper(user: User): UserResponse {
-    return UserResponse(user.id, user.login)
-}
+fun userMapper(user: User): UserResponse = UserResponse(user.id, user.login)
